4b7bd31d42480e54e0fc3e8283efffc6009aea13
Fabio <fabiojrodrigues.r@gmail.com>
2019-10-01 00:15:04 +0900

    adicionando design da tela de login

---
+++ layouts/login.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="h-100">
+    <nuxt />
+  </div>
+</template>
+
+<style lang="sass">
+html,
+body,
+#__nuxt,
+#__layout
+  height: 100%
+</style>
---
+++ pages/login.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="container h-100">
+    <div class="row h-100 justify-content-center align-items-center">
+      <logo class="login-logo" />
+      <b-form class="col-4 m-2">
+        <b-form-group id="username-group" label="Email:" label-for="username">
+          <b-form-input
+            id="username"
+            v-model="userName"
+            type="email"
+          ></b-form-input>
+        </b-form-group>
+
+        <b-form-group id="password-group" label="Senha:" label-for="password">
+          <b-form-input
+            id="password"
+            v-model="password"
+            type="password"
+          ></b-form-input>
+        </b-form-group>
+
+        <b-button class="float-right" type="submit" variant="primary"
+          >Login</b-button
+        >
+      </b-form>
+    </div>
+  </div>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+
+export default {
+  components: {
+    Logo
+  },
+  layout: 'login',
+  data() {
+    return {
+      userName: null,
+      password: null
+    }
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+.login-logo
+  margin-top: -78px
+</style>
