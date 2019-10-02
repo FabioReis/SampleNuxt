4298af037c5fb733668e9ac8dbe91cf495048e48
Fabio <fabiojrodrigues.r@gmail.com>
2019-10-03 00:12:30 +0900

    adicionando exemplo de mixin local juntamente com a utilização de moment js

--- nuxt.config.js
+++ nuxt.config.js
@@ -27,7 +27,7 @@ export default {
   /*
    ** Plugins to load before mounting the App
    */
-  plugins: ['~/plugins/repository'],
+  plugins: ['~/plugins/apiRepository'],
   /*
    ** Nuxt.js dev-modules
    */
